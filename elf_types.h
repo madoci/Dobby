f6c91e23e5ba3afc9870f6ee2e3dc99e4df1fcac
requiem958 <marius.monnier@etu.univ-grenoble-alpes.fr>
2018-12-17 10:35:24 +0100

    Add section types

--- elf_types.h
+++ elf_types.h
@@ -1,16 +1,18 @@
 #ifndef ELF_TYPES_H
 #define ELF_TYPES_H
 
+# include <stdint.h>
+
 /* Elf32 data types */
 
 /* Elf32 atomic types (page 8, figure 1-2, Format_ELF.pdf) */
 
-typedef signed long int Elf32_Sword; /* Size : 32 bits */
-typedef unsigned long int Elf32_Word;/* Size : 32 bits */
-typedef unsigned short int Elf32_Half;/*Size : 16 bits */
+typedef int32_t Elf32_Sword; /* Size : 32 bits */
+typedef uint32_t Elf32_Word;/* Size : 32 bits */
+typedef uint16_t Elf32_Half;/*Size : 16 bits */
 
-typedef unsigned long int Elf32_Off; /* Size : 32 bits */
-typedef unsigned long int Elf32_Addr;/* Size : 32 bits */
+typedef uint32_t Elf32_Off; /* Size : 32 bits */
+typedef uint32_t Elf32_Addr;/* Size : 32 bits */
 
 /* Elf32 file header type (page 9, figure 1-3, Format_ELF.pdf) */
 
@@ -36,8 +38,8 @@ typedef struct {
 
 //Indexes in the e_ident attribute.
 enum Elf_Ident {
-  EI_MAG0     = 0, // 0x7F
-  EI_MAG1     = 1, // 'E'
+  EI_MAG0       = 0, // 0x7F
+  EI_MAG1       = 1, // 'E'
   EI_MAG2	= 2, // 'L'
   EI_MAG3	= 3, // 'F'
   EI_CLASS	= 4, // Architecture (32/64)
@@ -105,6 +107,71 @@ enum Elf_Flags {
   EF_ARM_GCCMASK = 0x00400FFF,
   EF_ARM_ABI_FLOAT_HARD =0x00000400,
   EF_ARM_ABI_FLOAT_SOFT =0x00000200
+};
+
+/* Sections data types */
+
+//Undefined section
+#define SHN_UNDEF (0)
+
+typedef struct {
+  Elf32_Word sh_name; //Index into section header string table
+  Elf32_Word sh_type; //Section type
+  Elf32_Word sh_flags;
+  Elf32_Addr sh_addr;//If != 0, Adress where section must be placed in memory
+  Elf32_Word sh_offset;//Location of the section in bytes from beginning of file
+  Elf32_Word sh_size;//Section size in bytes
+  Elf32_Word sh_link;//See page 19, figure 1-13 Format_elf.pdf
+  Elf32_Word sh_info;//idem
+  Elf32_Word sh_addralign;//Address align constraints
+  Elf32_Word sh_entsize;//Size of entries if "symbol table" in section
+}Elf32_Shdr;
+
+//sh_types values
+enum Elf_ShType {
+  SHT_NULL      =0, //Inactive section
+  SHT_PROGBITS  =1, //Program section
+  SHT_SYMTAB    =2, //Symbol table (for link editing)
+  SHT_STRTAB    =3, //String table
+  SHT_RELA      =4, //Relocation entries + "addends"
+  SHT_HASH      =5, //Symbol hash table
+  SHT_DYNAMIC   =6, //Dynamic linking
+  SHT_NOTE      =7, //Note on program
+  SHT_NOBITS    =8, //Section without space in file
+  SHT_REL       =9, //Relocation entries without "addends"
+  SHT_SHLIB     =10,//Not conforming to abi
+  SHT_DYNSYM    =11,//Symbol table
+  //Specific to ARM
+  SHT_ARM_EXIDX =0x7000001, //Exception
+  SHT_ARM_PREEMPTMAP =0x7000002, //Dynamic linking bpapi dll
+  SHT_ARM_ATTRIBUTES =0x7000003, //Compatibility attributes
+  SHT_ARM_DEBUGOVERLAY =0x7000004,
+  SHT_ARM_OVERLAYSECTION = 0x7000005,
+  //Reserved
+  SHT_LOPROC    =0x7000000,
+  SHT_HIPROC    =0x7ffffff,
+  SHT_LOUSER    =0x8000000,
+  SHT_HIUSER    =0xfffffff
+};
+
+enum Elf_ShFlags {
+  SHF_WRITE    =0x1,
+  SHF_ALLOC    =0x2,
+  SHF_EXECINSTR=0x3,
+  SHF_MASKPROC =0xf0000000
+};
+
+const Elf32_Shdr entry0 = {
+  .sh_name = 0,
+  .sh_type = Elf_ShType.SHT_NULL,
+  .sh_flags= 0,
+  .sh_addr = 0,
+  .sh_offset=0,
+  .sh_size = 0,
+  .sh_link = SHN_UNDEF,
+  .sh_info = 0,
+  .sh_addralign = 0,
+  .sh_entsize = 0
 };
 
 
