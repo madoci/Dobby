be1f7f983c4df88e3977d16336d96b1711131915
Castraspin <laura.frebyp@gmail.com>
2018-12-17 14:35:46 +0100

    modification de la gestion des erreurs ^^

--- elf_header.c
+++ elf_header.c
@@ -2,38 +2,38 @@
 #include "elf_header.h"
 
 
-ERR_ELF_READER check_ident(Elf32_Ehdr* hdr){
+ERR_ELF_HEADER check_ident(Elf32_Ehdr* hdr){
   if(hdr->e_ident[EI_MAG0] != ELFMAG0 ||
      hdr->e_ident[EI_MAG1] != ELFMAG1 ||
      hdr->e_ident[EI_MAG2] != ELFMAG2 ||
      hdr->e_ident[EI_MAG3] != ELFMAG3){
-    return ERR_MAG;
+    return ERR_EH_IMAG;
   }
   if(hdr->e_ident[EI_CLASS] != ELFCLASSNONE &&
      hdr->e_ident[EI_CLASS] != ELFCLASS32  &&
      hdr->e_ident[EI_CLASS] != ELFCLASS64){
-    return ERR_CLASS;
+    return ERR_EH_ICLASS;
   }
   if(hdr->e_ident[EI_DATA] != ELFDATANONE &&
      hdr->e_ident[EI_DATA] != ELFDATA2LSB &&
      hdr->e_ident[EI_DATA] != ELFDATA2MSB){
-    return ERR_DATA;
+    return ERR_EH_IDATA;
   }
   if(hdr->e_ident[EI_VERSION] != EI_VALVERSION){
-    return ERR_VERSION;
+    return ERR_EH_IVERSION;
   }
   if(hdr->e_ident[EI_OSABI] != ELFOSABI_ARM_AEABI &&
      hdr->e_ident[EI_OSABI] != ELFOSABI_NONE){
-    return ERR_OSABI;
+    return ERR_EH_IOSABI;
   }
   if(hdr->e_ident[EI_PAD] != EI_VALPAD){
-    return ERR_PAD;
+    return ERR_EH_IPAD;
   }
-  return ERR_NONE;
+  return ERR_EH_NONE;
 }
 
 
-ERR_ELF_READER check_type(Elf32_Ehdr* hdr){
+ERR_ELF_HEADER check_type(Elf32_Ehdr* hdr){
   if(hdr->e_type != ET_NONE   &&
      hdr->e_type != ET_REL    &&
      hdr->e_type != ET_EXEC   &&
@@ -41,51 +41,51 @@ ERR_ELF_READER check_type(Elf32_Ehdr* hdr){
      hdr->e_type != ET_CORE   &&
      hdr->e_type != ET_LOPROC &&
      hdr->e_type != ET_HIPROC){
-    return 0;
+    return ERR_EH_TYPE;
   }
-  return 1;
+  return ERR_EH_NONE;
 }
 
 
-ERR_ELF_READER check_machine(Elf32_Ehdr* hdr){
+ERR_ELF_HEADER check_machine(Elf32_Ehdr* hdr){
   if(hdr->e_machine != EM_NONE &&
      hdr->e_machine != EM_ARM){
-    return 0;
+    return ERR_EH_MACHINE;
   }
-  return 1;
+  return ERR_EH_NONE;
 }
 
 
-ERR_ELF_READER check_version(Elf32_Ehdr* hdr){
+ERR_ELF_HEADER check_version(Elf32_Ehdr* hdr){
   if(hdr->e_version != EV_NONE &&
      hdr->e_version != EV_CURRENT){
-    return 0;
+    return ERR_EH_VERSION;
   }
-  return 1;
+  return ERR_EH_NONE;
 }
 
 
-ERR_ELF_READER check_flags(Elf32_Ehdr* hdr){
+ERR_ELF_HEADER check_flags(Elf32_Ehdr* hdr){
   if(hdr->e_flags != EF_ARM_ABIMASK        &&
      hdr->e_flags != EF_ARM_ABIVER         &&
      hdr->e_flags != EF_ARM_BE8            &&
      hdr->e_flags != EF_ARM_GCCMASK        &&
      hdr->e_flags != EF_ARM_ABI_FLOAT_HARD &&
      hdr->e_flags != EF_ARM_ABI_FLOAT_SOFT){
-    return 0;
+    return ERR_EH_FLAGS;
   }
-  return 1;
+  return ERR_EH_NONE;
 }
 
 
-ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
+ERR_ELF_HEADER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
 
   /* ELF IDENTIFICATION */
 
   fread(hdr->e_ident, 1, 16, f);
 
-  ERR_ELF_READER erreur = check_ident(hdr);
-  if(erreur != ERR_NONE){
+  ERR_ELF_HEADER erreur = check_ident(hdr);
+  if(erreur != ERR_EH_NONE){
     return erreur;
   }
 
@@ -94,7 +94,7 @@ ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
   fread(&(hdr->e_type), 2, 1, f);
 
   erreur = check_type(hdr);
-  if(erreur != ERR_NONE){
+  if(erreur != ERR_EH_NONE){
     return erreur;
   }
   
@@ -103,7 +103,7 @@ ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
   fread(&(hdr->e_machine), 2, 1, f);
 
   erreur = check_machine(hdr);
-  if(erreur != ERR_NONE){
+  if(erreur != ERR_EH_NONE){
     return erreur;
   }
 
@@ -111,7 +111,7 @@ ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
   
   fread(&(hdr->e_version), 4, 1, f);
   erreur = check_version(hdr);
-  if(erreur != ERR_NONE){
+  if(erreur != ERR_EH_NONE){
     return erreur;
   }
 
@@ -131,7 +131,7 @@ ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr){
 
   fread(&(hdr->e_flags), 4, 1, f);
   erreur = check_flags(hdr);
-  if(erreur != ERR_NONE){
+  if(erreur != ERR_EH_NONE){
     return erreur;
   }
 
--- elf_header.h
+++ elf_header.h
@@ -5,32 +5,36 @@
 
 #include <stdio.h>
 
-typedef enum ERR_ELF_READER{
-	ERR_NONE,
-	ERR_MAG,
-	ERR_CLASS,
-	ERR_DATA,
-	ERR_VERSION,
-	ERR_OSABI,
-	ERR_PAD
-}ERR_ELF_READER;
+typedef enum ERR_ELF_HEADER{
+	ERR_EH_NONE,
+	ERR_EH_IMAG,
+	ERR_EH_ICLASS,
+	ERR_EH_IDATA,
+	ERR_EH_IVERSION,
+	ERR_EH_IOSABI,
+	ERR_EH_IPAD,
+	ERR_EH_TYPE,
+	ERR_EH_MACHINE,
+	ERR_EH_VERSION,
+	ERR_EH_FLAGS
+}ERR_ELF_HEADER;
 
 /* check if the identification is in correct ELF format :
 	1 if true
 	0 if false */
-ERR_ELF_READER check_ident(Elf32_Ehdr* hdr);
+ERR_ELF_HEADER check_ident(Elf32_Ehdr* hdr);
 
-ERR_ELF_READER check_type(Elf32_Ehdr* hdr);
+ERR_ELF_HEADER check_type(Elf32_Ehdr* hdr);
 
-ERR_ELF_READER check_machine(Elf32_Ehdr* hdr);
+ERR_ELF_HEADER check_machine(Elf32_Ehdr* hdr);
 
-ERR_ELF_READER check_version(Elf32_Ehdr* hdr);
+ERR_ELF_HEADER check_version(Elf32_Ehdr* hdr);
 
-ERR_ELF_READER check_flags(Elf32_Ehdr* hdr);
+ERR_ELF_HEADER check_flags(Elf32_Ehdr* hdr);
 
 /* read an elf header in specified file and fill the given struct with
    f must be opened and hdr a correct pointer.
    return 0 if everything okay, -1 else*/
-ERR_ELF_READER read_elf_header(FILE *f, Elf32_Ehdr* hdr);
+ERR_ELF_HEADER read_elf_header(FILE *f, Elf32_Ehdr* hdr);
 
 #endif
