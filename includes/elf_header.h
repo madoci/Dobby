a93b2061cbbd550a05798f1557709d2a869c8af1
madoci <36835658+madoci@users.noreply.github.com>
2018-12-19 08:23:58 +0100

    Mise à jour de la valeur des erreurs

--- includes/elf_header.h
+++ includes/elf_header.h
@@ -6,17 +6,17 @@
 #include <stdio.h>
 
 typedef enum Err_ELF_Header{
-	ERR_EH_NONE,
-	ERR_EH_IMAG,
-	ERR_EH_ICLASS,
-	ERR_EH_IDATA,
-	ERR_EH_IVERSION,
-	ERR_EH_IOSABI,
-	ERR_EH_IPAD,
-	ERR_EH_TYPE,
-	ERR_EH_MACHINE,
-	ERR_EH_VERSION,
-	ERR_EH_FLAGS
+	ERR_EH_NONE     =  0,
+	ERR_EH_IMAG     =  1,
+	ERR_EH_ICLASS   =  2,
+	ERR_EH_IDATA    =  3,
+	ERR_EH_IVERSION =  4,
+	ERR_EH_IOSABI   =  5,
+	ERR_EH_IPAD     =  6,
+	ERR_EH_TYPE     =  7,
+	ERR_EH_MACHINE  =  8,
+	ERR_EH_VERSION  =  9,
+	ERR_EH_FLAGS    = 10
 } Err_ELF_Header;
 
 /* read an elf header in specified file and fill the given struct with
