8930500f37e95375ffcb939a94e5c780df25b140
madoci <36835658+madoci@users.noreply.github.com>
2018-12-19 08:25:44 +0100

    Changement de "read" en "display"

--- includes/elf_section_content.h
+++ includes/elf_section_content.h
@@ -5,7 +5,7 @@
 #include "elf_types_header.h"
 #include "elf_types_section.h"
 
-void read_elf_section_content_by_name(FILE *f, Elf32_Shdr tab[], Elf32_Ehdr hdr, char *name);
-void read_elf_section_content_by_num(FILE *f, Elf32_Shdr tab[], Elf32_Ehdr hdr, Elf32_Half num);
+void display_elf_section_content_by_name(FILE *f, Elf32_Shdr tab[], Elf32_Ehdr hdr, char *name);
+void display_elf_section_content_by_num(FILE *f, Elf32_Shdr tab[], Elf32_Ehdr hdr, Elf32_Half num);
 
 #endif
