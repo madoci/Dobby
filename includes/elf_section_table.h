aa19c0bfcc202f1f4df2a8809ab1d14413a2ef83
Castraspin <45362143+Castraspin@users.noreply.github.com>
2018-12-20 21:57:58 +0100

    Commentaires

--- includes/elf_section_table.h
+++ includes/elf_section_table.h
@@ -1,10 +1,13 @@
 #ifndef ELF_SECTIONS_TABLE_H
 #define ELF_SECTIONS_TABLE_H
+
 #include <stdio.h>
 #include "elf_types.h"
 
+/* Lis la table des entêtes de sections et remplis le tableau e_table */
 void read_elf_section_table(FILE *f, Elf32_Ehdr *header, Elf32_Shdr e_table[]);
 
+/* Lis la liste des entêtes de sections */
 void display_section_header(FILE* f);
 
 #endif
