a8173d1a8e348bc87473cd868af6d9ee823b78ef
Castraspin <45362143+Castraspin@users.noreply.github.com>
2018-12-21 00:04:52 +0100

    Nettoyage et correction

--- includes/elf_relocation.h
+++ includes/elf_relocation.h
@@ -1,5 +1,5 @@
-#ifndef ELF_RELOCATIONS_H
-#define ELF_RELOCATIONS_H
+#ifndef ELF_RELOCATION_H
+#define ELF_RELOCATION_H
 
 #include <stdio.h>
 #include "elf_types_header.h"
@@ -12,7 +12,7 @@ void read_rel_section(FILE* f, Elf32_Shdr *hdr, Elf32_Rel entries[]);
 /* Read the relocation table of a RELA header section */
 void read_rela_section(FILE* f, Elf32_Shdr *hdr, Elf32_Rela entries[]);
 
+/* Display all relocation tables of an ELF file */
 void display_all_relocation_table(FILE* f, Elf32_Ehdr *hdr, Elf32_Shdr shdr[]);
-
 
 #endif
