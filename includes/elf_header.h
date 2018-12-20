96eba37b8ade665688742889c95de74a67b0e9c7
Castraspin <45362143+Castraspin@users.noreply.github.com>
2018-12-20 23:24:30 +0100

    Nettoyage et correction

--- includes/elf_header.h
+++ includes/elf_header.h
@@ -19,26 +19,10 @@ typedef enum Err_ELF_Header{
 	ERR_EH_FLAGS    = 10
 } Err_ELF_Header;
 
-#define printf_8x(s,x) printf("\t%-50s\t0x%08x\n",s,x)
-#define printf_d(s,d) printf("\t%-50s\t%d\n",s,d)
-#define printf_s(s,s2) printf("\t%-50s\t%s\n",s,s2)
-#define printf_se(s) printf("\t%-50s\t",s)
-
-
+/* Read the header of an ELF file and in the hdr argument */
 Err_ELF_Header read_elf_header(FILE *f, Elf32_Ehdr* hdr);
 
+/* Display an ELF header */
 void display_header(Elf32_Ehdr * Elf);
-void display_ident(unsigned char e_ident[]);
-
-void display_class(unsigned char class);
-void display_data(unsigned char data);
-void display_eiVersion(unsigned char version);
-void display_osabi(unsigned char osabi);
-void display_type(Elf32_Half type);
-void display_machine(Elf32_Half machine);
-void display_fileVersion(Elf32_Word version);
-void display_verAbi(Elf32_Word flag);
-
-void display_flags(Elf32_Word flag);
 
 #endif
