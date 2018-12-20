f28fe048d9b67eee6e121beeff02d5cbd2303c86
Castraspin <45362143+Castraspin@users.noreply.github.com>
2018-12-21 00:05:04 +0100

    Nettoyage et correction

--- elf_relocation.c
+++ elf_relocation.c
@@ -1,7 +1,10 @@
 #include "elf_relocation.h"
-#include "fread.h"
-#include "elf_string_table.h"
 
+#include "fread.h"
+#include "elf_section_content.h"
+
+
+/* READ RELOCATION TABLE */
 
 Elf32_Rel read_rel(FILE* f){
   Elf32_Rel r;
@@ -22,7 +25,7 @@ Elf32_Rela read_rela(FILE* f){
 void read_rel_section(FILE* f, Elf32_Shdr *hdr, Elf32_Rel entries[]){
   unsigned int i;
   for (i=0; i<(hdr->sh_size/sizeof(Elf32_Rel)); i++){
-    fseek(f, hdr->sh_offset+i*sizeof(Elf32_Rel), SEEK_SET);
+    fseek(f, hdr->sh_offset + i * sizeof(Elf32_Rel), SEEK_SET);
     entries[i] = read_rel(f);
   }
 }
@@ -35,70 +38,73 @@ void read_rela_section(FILE* f, Elf32_Shdr *hdr, Elf32_Rela entries[]){
   }
 }
 
+
+/* DISPLAY RELOCATION TABLE */
+
 const char * relocation_type(Elf_RelT type){
-	switch (type){
-		case R_386_NONE:
-			return "R_386_NONE";
-		case R_386_32:
-			return "R_386_32:";
-		case R_ARM_ABS32:
-			return "R_ARM_ABS32";
-		case R_ARM_ABS16:
-			return "R_ARM_ABS16";
-		case R_ARM_ABS12:
-			return "R_ARM_ABS12";
-		case R_ARM_ABS8:
-			return "R_ARM_ABS8";
-		case R_ARM_ABS32_NOI:
-			return "R_ARM_ABS32_NOI";
-		case R_ARM_JUMP24:
-			return "R_ARM_JUMP24";
-		case R_ARM_CALL:
-			return "R_ARM_CALL";
-		default:
-			return "erreurType";
-	}
+  switch (type){
+    case R_386_NONE:
+      return "R_386_NONE";
+    case R_386_32:
+      return "R_386_32:";
+    case R_ARM_ABS32:
+      return "R_ARM_ABS32";
+    case R_ARM_ABS16:
+      return "R_ARM_ABS16";
+    case R_ARM_ABS12:
+      return "R_ARM_ABS12";
+    case R_ARM_ABS8:
+      return "R_ARM_ABS8";
+    case R_ARM_ABS32_NOI:
+      return "R_ARM_ABS32_NOI";
+    case R_ARM_JUMP24:
+      return "R_ARM_JUMP24";
+    case R_ARM_CALL:
+      return "R_ARM_CALL";
+    default:
+      return "erreurType";
+  }
 }
 
 void display_rel_table(FILE* f, Elf32_Shdr *shdr){
-	Elf32_Rel entries[shdr->sh_size/sizeof(Elf32_Rel)];
-	read_rel_section(f, shdr, entries);
+  Elf32_Rel entries[shdr->sh_size/sizeof(Elf32_Rel)];
+  read_rel_section(f, shdr, entries);
 
   printf(" %-8s  %-8s  %-15s  %-8s\n",
          "Decalage", "Info", "Type", "Ind.-sym");
 
-	unsigned int i;
-	for (i=0; i<shdr->sh_size/sizeof(Elf32_Rel); i++){
+  unsigned int i;
+  for (i=0; i<shdr->sh_size/sizeof(Elf32_Rel); i++){
     const Elf32_Addr decal = entries[i].r_offset;
     const Elf32_Word inf = entries[i].r_info;
     const char *type = relocation_type(ELF32_R_TYPE(entries[i].r_info));
     const Elf32_Word ind = ELF32_R_SYM(entries[i].r_info);
     printf(" %08x  %08x  %-15s  %-8d\n",
            decal, inf, type, ind);
-	}
+  }
 }
 
 void display_rela_table(FILE* f, Elf32_Shdr *shdr){
-	Elf32_Rela entries[shdr->sh_size/sizeof(Elf32_Rela)];
-	read_rela_section(f, shdr, entries);
+  Elf32_Rela entries[shdr->sh_size / sizeof(Elf32_Rela)];
+  read_rela_section(f, shdr, entries);
 
   printf(" %-8s  %-8s  %-15s  %-8s  %-8s\n",
          "Decalage", "Info", "Type", "Ind.-sym", "Addend");
 
-	unsigned int i;
-	for (i=0; i<shdr->sh_size/sizeof(Elf32_Rel); i++){
+  unsigned int i;
+  for (i=0; i<shdr->sh_size/sizeof(Elf32_Rel); i++){
     const Elf32_Addr decal = entries[i].r_offset;
     const Elf32_Word inf = entries[i].r_info;
     const char *type = relocation_type(ELF32_R_TYPE(entries[i].r_info));
     const Elf32_Word ind = ELF32_R_SYM(entries[i].r_info);
-    const Elf32_Sword	addend = entries[i].r_addend;
+    const Elf32_Sword addend = entries[i].r_addend;
     printf(" %08x  %08x  %-15s  %-8d  %-8d\n",
            decal, inf, type, ind, addend);
-	}
+  }
 }
 
 void display_all_relocation_table(FILE* f, Elf32_Ehdr *hdr, Elf32_Shdr shdr[]){
-  char *str_table = extract_string_table(f, shdr[hdr->e_shstrndx]);
+  char *str_table = read_elf_section_content(f, shdr[hdr->e_shstrndx]);
 
   unsigned int i;
   for (i=0; i<hdr->e_shnum; i++){
