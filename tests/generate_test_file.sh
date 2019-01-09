ffb25cf772fee1a58ff71c04276535b6cda4b1e6
luci <chlous.clem@gmail.com>
2019-01-09 13:31:37 +0100

    Ajout de jeux de tests

--- tests/generate_test_file.sh
+++ tests/generate_test_file.sh
@@ -1,4 +1,4 @@
-CC="arm-none-eabi-gcc -mbig-endian -mno-thumb-interwork -O0 "
+CC="arm-linux-gnueabi-gcc -mbig-endian -mno-thumb-interwork -O0 "
 CC_COMP="-c"
 CC_S="-S"
 CC_E="-nostdlib --entry main -n -Wl,--section-start -Wl,.text=0x20 -Wl,--section-start -Wl,.data=0x2800 -Wl,-EB"
