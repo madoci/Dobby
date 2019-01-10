e0e107170be466aac1c7526426ddee4c97ee3376
Castraspin <laura.frebyp@gmail.com>
2019-01-10 11:36:04 +0100

    arrrrrgh

--- includes/elf_load_symbol.h
+++ includes/elf_load_symbol.h
@@ -9,8 +9,4 @@
 /* */
 void correct_all_symbol(Elf32_File* ef, Elf32_Half sh_num, Elf32_Half correl_table[], Elf32_Half correl_symbol[]);
 
-<<<<<<< HEAD
-void correct_all_symbol(Elf32_File* ef, Elf32_Half sh_num, Elf32_Half correl_table[], Elf32_Half correl_symbol[]);
-=======
 #endif
->>>>>>> 520d69ec29cf57cf8bf021c0e6815ac633a8a29c
